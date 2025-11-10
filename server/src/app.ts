538d83165092a72c0c62f623c2d4bf1fe8c95f44
roro86416 <g650azy@gmail.com>
2025-11-10 16:45:36 +0800

    合併合併dev/4toast_2

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,6 +14,7 @@ import eventRatingsRoutes from "./modules/event-ratings/event-ratings.routes.js"
 import cartRoutes from "./modules/cart/cart.routes.js";
 
 dotenv.config();
+
 const app: Express = express();
 
 // --- 中間件 (Middlewares) ---
